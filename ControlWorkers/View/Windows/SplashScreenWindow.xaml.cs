b4a33a83a371fd0e113337260d700df5c815bf94
Dmitry <Dmitry@DESKTOP-VPBJ7PR>
2022-10-29 23:25:57 +0500

    Добавьте файлы проекта.

---
+++ ControlWorkers/View/Windows/SplashScreenWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControlWorkers.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для SplashScreenWindow.xaml
+    /// </summary>
+    public partial class SplashScreenWindow : Window
+    {
+        public SplashScreenWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            if (progressBar.Value == 100)
+            {
+                AuthWindow authWindow = new AuthWindow();
+                Close();
+                authWindow.ShowDialog();
+            }
+        }
+
+        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+
+            for (int i = 0; i <= 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(40);
+            }
+        }
+    }
+}
