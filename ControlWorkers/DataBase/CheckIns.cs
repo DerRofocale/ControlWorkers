16d66e64706e4fc0ca275bc70e6538037c1966bf
Дмитрий Яльчик <dmitry_yalchik@mail.ru>
2022-10-31 02:06:12 +0500

    AddedDataClasses

---
+++ ControlWorkers/DataBase/CheckIns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWorkers.DataBase
+{
+    public class CheckIns
+    {
+        public int Id { get; set; }
+        public DateTime DateTimeRegister { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ ControlWorkers/Services/RegistryService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ControlWorkers.Services
+{
+    public class RegistryService
+    {
+        #region SETTINGS
+        private static readonly string registryPathSettings = Path.Combine(Registry.CurrentUser.Name, "SOFTWARE\\ControlWorkers");
+        public static string GetRegistryKeySettings(string key)
+        {
+            return (string)Registry.GetValue(registryPathSettings, key, string.Empty);
+        }
+        public static void SetRegistryKeySettings(string key, string value)
+        {
+            Registry.SetValue(registryPathSettings, key, value, RegistryValueKind.String);
+        }
+        #endregion
+
+        #region USER
+        private static readonly string registryPathUser = Path.Combine(Registry.CurrentUser.Name, "SOFTWARE\\ControlWorkers\\CurrentUser");
+        public static string GetRegistryKeyUser(string key)
+        {
+            return (string)Registry.GetValue(registryPathUser, key, string.Empty);
+        }
+        public static void SetRegistryKeyUser(string key, string value)
+        {
+            Registry.SetValue(registryPathUser, key, value, RegistryValueKind.String);
+        }
+        #endregion
+    }
+}
